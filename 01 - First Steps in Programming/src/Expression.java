ffffb3c2563003b82530f595ff87b54ede9db6ca
Ivo Hristov <i_hrisstov@hotmail.com>
2017-04-26 13:42:29 +0300

    Added First steps in Programming

---
+++ 01 - First Steps in Programming/src/Expression.java
@@ -0,0 +1,9 @@
+/**
+ * Created by User on 4.3.2017 г..
+ */
+public class Expression {
+    public static void main(String[] args) {
+        int result = (3522 + 52353) * 23 - (2336 * 501 + 23432 - 6743) * 3;
+        System.out.println(result);
+    }
+}
