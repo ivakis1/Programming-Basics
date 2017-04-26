61af7dc53b2a0649ce7d2e4ff9a7fe73a5d5715b
Ivo Hristov <i_hrisstov@hotmail.com>
2017-04-26 13:20:12 +0300

    01. Course intro

---
+++ CourseIntro/src/Retriangle.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Retriangle {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int a = Integer.parseInt(scanner.nextLine());
+        int b = Integer.parseInt(scanner.nextLine());
+
+        int result  = a * b;
+        System.out.println(result);
+    }
+}
---
+++ CourseIntro/src/StarBox.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+/**
+ * Created by User on 5.3.2017 г..
+ */
+public class StarBox {
+    public static void main(String[] args) {
+        Scanner console =  new Scanner(System.in);
+        int n = Integer.parseInt(console.nextLine())
+    }
+}
